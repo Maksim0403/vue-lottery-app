585293c02479ad84856014fe723a368a75075d7b
Maksim0403 <maksim.didychuk4@gmail.com>
2025-10-29 22:23:59 +0200

    Add 2 templates: modal, search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+
+const emit = defineEmits<{
+  (e: 'filter-by-name', query: string): void
+}>()
+
+</script>
+
+<template>
+<input
+  type="text"
+  class="form-control mb-3"
+  placeholder="Search participants by name..."
+  v-model="searchQuery"
+  @input="emit('filter-by-name', searchQuery)">
+</template>
+
+<style scoped>
+
+</style>
