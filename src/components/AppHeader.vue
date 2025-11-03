af97995cba79c9a9ab5a1542b81d8741ed166ac9
Maksim0403 <maksim.didychuk4@gmail.com>
2025-11-03 17:27:09 +0200

    Add components AppHeader and AppNavigation

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AppNavigation from '@/components/AppNavigation.vue'
+</script>
+
+<template>
+<header class="mb-2">
+  <h1 class="text-center">Lottery App</h1>
+</header>
+  <AppNavigation />
+</template>
---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { logout } from '@/repositories/UserRepository'
+import BaseButton from '@/components/BaseButton.vue'
+
+const router = useRouter()
+const isAuthenticated = ref(!!localStorage.getItem('token'))
+
+window.addEventListener('storage', () => {
+  isAuthenticated.value = !!localStorage.getItem('token')
+})
+
+router.afterEach(() => {
+  isAuthenticated.value = !!localStorage.getItem('token')
+})
+
+const handleLogout = () => {
+  logout()
+  isAuthenticated.value = false
+  router.push('/login')
+}
+</script>
+
+<template>
+  <nav class="d-flex align-items-center gap-2 mb-4">
+
+    <router-link
+      class="btn"
+      active-class="btn-primary text-white"
+      :to="{ name: 'home' }"> Home </router-link>
+
+    <router-link
+      class="btn"
+      active-class="btn-primary text-white"
+      :to="{ name: 'about' }">About</router-link>
+
+    <router-link
+      class="btn"
+      active-class="btn-primary text-white"
+      :to="{ name: 'lottery' }">Lottery</router-link>
+
+    <router-link
+      class="btn"
+      active-class="btn-primary text-white"
+      :to="{ name: 'user', params: { id: 1 } }">Users</router-link>
+
+    <router-link
+      v-if="!isAuthenticated"
+      class="btn btn-primary ms-auto"
+      :to="{ name: 'login' }"
+    >
+      Login
+    </router-link>
+
+    <BaseButton
+      v-else
+      variant="danger"
+      class="ms-auto"
+      label="Logout"
+      @click="handleLogout"
+    />
+
+  </nav>
+</template>
